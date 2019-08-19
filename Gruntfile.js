d197e50cb0f5e095027497f911bc8504be3b1243
navanath <navanathb@bsf.io>
2019-08-19 19:59:58 +0530

    readme file generated

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,6 +73,14 @@ module.exports = function (grunt) {
                 }
             }
         },
+
+        wp_readme_to_markdown: {
+			your_target: {
+				files: {
+					"README.md": "readme.txt"
+				}
+			},
+		},
         
         addtextdomain: {
             options: {
@@ -83,13 +91,19 @@ module.exports = function (grunt) {
                     src: ['*.php', '**/*.php', '!node_modules/**', '!php-tests/**', '!bin/**', '!asset/bsf-core/**']
                 }
             }
-        }
+        },
     });
 
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-wp-i18n');
+
+    /* Read File Generation task */
+    grunt.loadNpmTasks("grunt-wp-readme-to-markdown");
+
+    // Generate Read me file
+	grunt.registerTask( "readme", ["wp_readme_to_markdown"] );
 
     grunt.registerTask('i18n', ['addtextdomain', 'makepot']);
     grunt.registerTask('release', ['clean:zip', 'copy', 'compress', 'clean:main']);
